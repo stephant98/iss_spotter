efedc9d84cec3803839eeec71e2a0a1963c1afe2
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-17 18:36:56 -0500

    refactored iss to use promises

---
+++ iss_promised.js
@@ -0,0 +1,34 @@
+const request = require('request-promise-native');
+
+
+const fetchMyIP = function () {
+  return request('https://api.ipify.org?format=json')
+};
+
+const fetchCoordsByIP = function(resultFromFetchMyIP) {
+  const ip = JSON.parse(resultFromFetchMyIP).ip
+  return request(`https://freegeoip.app/json/${ip}`)
+ 
+};
+
+const fetchISSFlyOverTimes = function(body) {
+  const latitude = JSON.parse(body).latitude;
+  const longitude = JSON.parse(body).longitude
+  const url = `http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`
+
+  return request(url);
+}
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((body) => {
+      const response = JSON.parse(body).response;
+      return response;
+    });
+}
+
+
+
+module.exports = { nextISSTimesForMyLocation };
