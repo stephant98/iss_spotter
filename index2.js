10881d0f9fcc8972b835b01d48d0b8ad11054eab
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-17 18:37:37 -0500

    refacored index file to use promises

---
+++ index2.js
@@ -0,0 +1,23 @@
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const dateTime = new Date(0);
+    dateTime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${dateTime} for ${duration} seconds!`);
+  }
+};
+
+
+
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message)
+  })
